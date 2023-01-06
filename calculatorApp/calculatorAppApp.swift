de25bc5a6cfa6720c597180a39668e044dbf0093
Wonkeun No <zakk23v@hotmail.com>
2023-01-06 10:31:03 -0500

    Initialize project

---
+++ calculatorApp/calculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorAppApp.swift
+//  calculatorApp
+//
+//  Created by Wonkeun No on 2023-01-06.
+//
+
+import SwiftUI
+
+@main
+struct calculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
