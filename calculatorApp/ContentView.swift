f490c8b6223157334fe233139f440c835b5bad2e
Wonkeun No <zakk23v@hotmail.com>
2023-01-06 18:17:59 -0500

    Add button layout

--- calculatorApp/ContentView.swift
+++ calculatorApp/ContentView.swift
@@ -1,14 +1,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black)
+            VStack {
+                TotalText()
+                HStack {
+                    CalculatorButton(buttonText: "1")
+                    CalculatorButton(buttonText: "2")
+                    CalculatorButton(buttonText: "3")
+                    CalculatorButton(buttonText: "+", color: .orange)
+                }
+                HStack {
+                    CalculatorButton(buttonText: "4")
+                    CalculatorButton(buttonText: "5")
+                    CalculatorButton(buttonText: "6")
+                    CalculatorButton(buttonText: "-", color: .orange)
+                }
+                HStack {
+                    CalculatorButton(buttonText: "7")
+                    CalculatorButton(buttonText: "8")
+                    CalculatorButton(buttonText: "9")
+                    CalculatorButton(buttonText: "X", color: .orange)
+                }
+                HStack {
+                    CalculatorButton(buttonText: "0", width: 148)
+                    CalculatorButton(buttonText: "C", color: .gray)
+                    CalculatorButton(buttonText: "=", color: .orange)
+                }
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
